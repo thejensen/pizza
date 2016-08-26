b661c14c0288450fad52ea41568f18950b0f14bc
Sara Jensen <sara.jensen@idealist.org>
2016-08-26 11:41:15 -0700

    push ingredients and size into an object, style jumbotron with banner color red, change font in body to Open Sans Condensed, add Price div.

--- js/scripts.js
+++ js/scripts.js
@@ -4,21 +4,19 @@ function Pizza(toppings, size) {
   this.toppings = [];
   this.size = [];
 }
-//
-// var basePrice = 10
-//
-// Pizza.prototype.price = function () {
-//   for (var i = 0; i < pizza.toppingsArray.length; i++) {
-//     basePrice += 1
-//   }
-//   for (var i = 0; i < pizza.size.length; i++) {
-//     pizza.size[i]
-//   }
-// }
+
+var basePrice = 10
+
+Pizza.prototype.price = function () {
+  for (var i = 0; i < pizza.toppingsArray.length; i++) {
+    basePrice += 1
+  }
+  for (var i = 0; i < pizza.size.length; i++) {
+    pizza.size[i]
+  }
+}
 
 // User Interface Logic
-//
-// var newPizza = new Pizza();
 
 $(document).ready(function() {
   $('form').submit(function(event) {
@@ -26,15 +24,13 @@ $(document).ready(function() {
 
     var newPizza = new Pizza();
 
-    var toppingsArray = []
-
     var inputtedToppings = $('.form-check-input:checked').map(function() {
-      toppingsArray.push($(this).val());
+      newPizza.toppings.push($(this).val());
     });
-    var inputtedSize = $('input:radio[name="size"]:checked').val();
+    newPizza.size.push($('input:radio[name="size"]:checked').val());
 
 
-    console.log(toppingsArray, inputtedSize);
+    console.log(newPizza);
 
   });
 });

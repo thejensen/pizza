544d8dc7396b72311e2ac61e1007c0593f9f81a2
Sara Jensen <sara.jensen@idealist.org>
2016-08-26 11:04:02 -0700

    add logic to grab checked ingredients to put in an array and store pizza size in a variable. Change header font and add funky pizza cartoon pattern to jumbotron.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,40 @@
-$(document).ready(function() {
+// Business Logic
 
-function Pizza() {
+function Pizza(toppings, size) {
   this.toppings = [];
   this.size = [];
 }
-var basePrice = 10
-Pizza.prototype.price = function () {
-  for (var i = 0; i < pizza.toppings.length; i++) {
-      var basePrice += 1
-    }
-  }
+//
+// var basePrice = 10
+//
+// Pizza.prototype.price = function () {
+//   for (var i = 0; i < pizza.toppingsArray.length; i++) {
+//     basePrice += 1
+//   }
+//   for (var i = 0; i < pizza.size.length; i++) {
+//     pizza.size[i]
+//   }
+// }
 
-var newPizza = new Pizza(toppings, size);
+// User Interface Logic
+//
+// var newPizza = new Pizza();
+
+$(document).ready(function() {
+  $('form').submit(function(event) {
+    event.preventDefault();
+
+    var newPizza = new Pizza();
+
+    var toppingsArray = []
+
+    var inputtedToppings = $('.form-check-input:checked').map(function() {
+      toppingsArray.push($(this).val());
+    });
+    var inputtedSize = $('input:radio[name="size"]:checked').val();
 
 
+    console.log(toppingsArray, inputtedSize);
 
-console.log()
-
-};
+  });
 });
